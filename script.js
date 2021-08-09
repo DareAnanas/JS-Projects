375e14be5153fc19f3471a61230d0ae225ff2928
DareAnanas <nylo2005@gmail.com>
2021-08-09 20:15:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let money;
+let time;
+let expenses;
+let appData = {
+    money: '',
+    time: '',
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: true,
+}
+
+
+function getExpenses() {
+    return [prompt("Введіть обов'язкову статтю витрат в цьому місяці?"),
+    prompt("У скільки обійдеться?")]
+}
+
+
+money = prompt('Ваш бюджет на місяць?', '')
+time = prompt('Введіть дату в форматі YYYY-MM-DD', '')
+appData.money = money
+appData.time = time
+expenses = getExpenses()
+appData.expenses[expenses[0]] = expenses[1]
+expenses = getExpenses()
+appData.expenses[expenses[0]] = expenses[1]
+console.log(appData.expenses)
+
+
+alert()
+
